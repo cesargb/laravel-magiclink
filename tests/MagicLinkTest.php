f33d6b5cdef8237a18aa0d3f191b62b80b402cf9
César Garcia <cesar@descom.es>
2018-03-27 14:20:18 +0200

    test add verify login as

--- tests/MagicLinkTest.php
+++ tests/MagicLinkTest.php
@@ -6,45 +6,40 @@ use Cesargb\MagicLink\MagicLink;
 
 class MagicLinkTest extends TestCase
 {
-    /** @test **/
-    public function it_can_create_magiclink()
+    public function test_it_can_create_magiclink()
     {
         $this->assertContains('/magiclink/', $this->testUser->create_magiclink(5));
     }
 
-    /** @test **/
-    public function it_can_get_magiclink()
+    public function test_it_can_get_magiclink()
     {
         $this->testUser->create_magiclink(5);
         $this->assertEquals($this->testUser->magiclinks()->count(), 1);
     }
 
-    /** @test **/
-    public function it_can_delete_all_magiclink()
+    public function test_it_can_delete_all_magiclink()
     {
         $magiclink = new MagicLink();
         $magiclink->delete_all();
         $this->assertTrue(true);
     }
 
-    /** @test **/
-    public function it_can_delete_expired_magiclink()
+    public function test_it_can_delete_expired_magiclink()
     {
         $magiclink = new MagicLink();
         $magiclink->delete_expired();
         $this->assertTrue(true);
     }
 
-    /** @test **/
-    public function it_can_get_magiclink2()
+    public function test_it_can_get_magiclink2()
     {
         $url = $this->testUser->create_magiclink(5);
         $response = $this->get($url);
         $response->assertStatus(302);
+        $this->assertEquals(auth()->user()->id,$this->testUser->id);
     }
 
-    /** @test **/
-    public function url_error_return_403()
+    public function test_url_error_return_403()
     {
         $response = $this->get('magiclink/error');
         $response->assertStatus(403);
