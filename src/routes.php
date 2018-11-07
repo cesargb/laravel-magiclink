b6f953867c8f50dbb24b311d9a53bc096da5a799
Kevin Redman <iredmedia@gmail.com>
2018-11-07 13:47:49 -0800

    Remove closures and use controller

--- src/routes.php
+++ src/routes.php
@@ -2,17 +2,6 @@
 
 
 Route::group(['middleware' => ['web']], function () {
-    Route::any(config('magiclink.url.redirect_error', 'magiclink/error'), function () {
-        abort(403);
-    });
-
-    Route::any(config('magiclink.url.validate_path', 'magiclink').'/{token}', function ($token) {
-        $MagicLink = new \Cesargb\MagicLink\MagicLink();
-        $result = $MagicLink->auth($token);
-        if ($result == false) {
-            return redirect(config('magiclink.url.redirect_error', '/magiclink/error'));
-        } else {
-            return redirect($result);
-        }
-    });
+    Route::any(config('magiclink.url.redirect_error', 'magiclink/error'), "Controllers/MagicLinkController@error");
+    Route::any(config('magiclink.url.validate_path', 'magiclink').'/{token}', "Controllers/MagicLinkController@validate" );
 });
