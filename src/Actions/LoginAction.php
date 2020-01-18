0a923a7219bd96fbe17cbc413b55d137f67e290f
Cesar <cesargb@gmail.com>
2020-01-18 11:51:59 +0100

    add feature mixed response in login action

--- src/Actions/LoginAction.php
+++ src/Actions/LoginAction.php
@@ -2,7 +2,11 @@
 
 namespace Cesargb\MagicLink\Actions;
 
+use Closure;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\View\View;
+use Opis\Closure\SerializableClosure;
+use Symfony\Component\HttpFoundation\Response;
 
 class LoginAction implements ActionInterface
 {
@@ -11,7 +15,7 @@ class LoginAction implements ActionInterface
      */
     protected $user;
 
-    protected $redirectTo;
+    protected $response;
 
     protected $guard;
 
@@ -19,15 +23,43 @@ class LoginAction implements ActionInterface
      * Constructor to action.
      *
      * @param \Illuminate\Database\Eloquent\Model $user
-     * @param string $redirectTo
+     * @param string|\Symfony\Component\HttpFoundation\Response $response
      */
-    public function __construct($user, $redirectTo = '/', $guard = 'web')
+    public function __construct($user, $response = null, $guard = 'web')
     {
         $this->user = $user;
 
-        $this->redirectTo = $redirectTo;
+        $this->response = $this->serializeResponse($response);
 
         $this->guard = $guard;
+    }
+
+    protected function serializeResponse($response)
+    {
+        $response = $this->formattedResponse($response);
+
+        if ($response instanceof Closure) {
+            return serialize(new SerializableClosure($response));
+        }
+
+        if ($response instanceof View) {
+            return serialize($response->render());
+        }
+
+        return serialize($response);
+    }
+
+    protected function formattedResponse($response)
+    {
+        if (is_null($response)) {
+            return redirect('/');
+        }
+
+        if (is_callable($response)) {
+            return Closure::fromCallable($response);
+        }
+
+        return $response;
     }
 
     /**
@@ -37,6 +69,21 @@ class LoginAction implements ActionInterface
     {
         Auth::guard($this->guard)->login($this->user);
 
-        return redirect($this->redirectTo);
+        return $this->callResponse(unserialize($this->response));
+    }
+
+    protected function callResponse($response)
+    {
+        if ($response instanceof Response) {
+            return $response;
+        }
+
+        if (is_callable($response)) {
+            return $response();
+        }
+
+        if (is_string($response)) {
+            return $response;
+        }
     }
 }
--- tests/Actions/LoginTest.php
+++ tests/Actions/LoginTest.php
@@ -9,7 +9,7 @@ use Cesargb\MagicLink\Test\User;
 
 class LoginTest extends TestCase
 {
-    public function test_auth()
+    public function test_auth_null_response()
     {
         $magiclink = MagicLink::create(new LoginAction(User::first()));
 
@@ -20,13 +20,63 @@ class LoginTest extends TestCase
         $this->assertAuthenticatedAs(User::first());
     }
 
-    public function test_auth_and_redirect_to_other_link()
+    public function test_auth_with_response_callable()
     {
-        $magiclink = MagicLink::create(new LoginAction(User::first(), '/test'));
+        $magiclink = MagicLink::create(
+            new LoginAction(User::first(),
+            function () {
+                return 'callback called';
+            }
+        ));
+
+        $this->get($magiclink->url)
+                ->assertStatus(200)
+                ->assertSeeText('callback called');
+
+        $this->assertAuthenticatedAs(User::first());
+    }
+
+    public function test_auth_with_response_redirect()
+    {
+        $magiclink = MagicLink::create(
+            new LoginAction(User::first(), redirect('/test'))
+        );
 
         $this->get($magiclink->url)
                 ->assertStatus(302)
                 ->assertRedirect('/test');
+
+        $this->assertAuthenticatedAs(User::first());
+    }
+
+    public function test_auth_with_response_view()
+    {
+        $magiclink = MagicLink::create(
+            new LoginAction(
+                User::first(),
+                view('data', ['data' => 'Lorem, ipsum dolor.'])
+            )
+        );
+
+        $this->get($magiclink->url)
+                ->assertStatus(200)
+                ->assertSeeText('Lorem, ipsum dolor.');
+
+        $this->assertAuthenticatedAs(User::first());
+    }
+
+    public function test_auth_with_response_string()
+    {
+        $magiclink = MagicLink::create(
+            new LoginAction(
+                User::first(),
+                'Lorem ipsum dolor sit'
+            )
+        );
+
+        $this->get($magiclink->url)
+                ->assertStatus(200)
+                ->assertSeeText('Lorem ipsum dolor sit');
 
         $this->assertAuthenticatedAs(User::first());
     }
