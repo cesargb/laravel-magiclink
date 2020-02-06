1be7f7638dd7185ee700614aff47ec3831b88774
robindrost <robindrost@gmail.com>
2020-02-06 19:33:48 +0100

    Update MagicLinkServiceProvider.php
    Load migrations instead of publishing them as described in the docs https://laravel.com/docs/5.3/packages#migrations. This will also make the migrations run while writing tests.

--- src/MagicLinkServiceProvider.php
+++ src/MagicLinkServiceProvider.php
@@ -16,10 +16,8 @@ class MagicLinkServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__.'/../config/magiclink.php' => config_path('magiclink.php'),
         ], 'config');
-
-        $this->publishes([
-            __DIR__.'/../databases/migrations/create_table_magic_links.php' => database_path('migrations').'/2017_07_06_000000_create_table_magic_links.php',
-        ], 'migrations');
+        
+        $this->loadMigrationsFrom(__DIR__.'/../databases/migrations');
 
         $this->loadRoutesFrom(__DIR__.'/routes.php');
     }
