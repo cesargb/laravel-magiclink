86c7b753eb2daea38194f2aea497058fc730236d
Cesar <cesargb@gmail.com>
2023-01-05 12:20:36 +0100

    add controller action

---
+++ tests/TestSupport/MyController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace MagicLink\Test\TestSupport;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+
+class MyController extends Controller
+{
+    public function __invoke()
+    {
+        return 'im a controller invoke';
+    }
+
+    public function index()
+    {
+        return 'im a controller index';
+    }
+}
