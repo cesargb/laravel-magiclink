af53d515fdac543d670dafd9d70053d0a7b6e3ff
Cesar <cesargb@gmail.com>
2025-01-04 14:58:21 +0100

    refactor service provider

--- src/MagicLinkServiceProvider.php
+++ src/MagicLinkServiceProvider.php
@@ -13,6 +13,35 @@ class MagicLinkServiceProvider extends ServiceProvider
 
     public function boot()
     {
+        $this->offerPublishing();
+
+        $this->loadRouteMagicLink();
+
+        $this->loadViewMagicLink();
+    }
+
+    private function loadRouteMagicLink(): void
+    {
+        $disableRegisterRoute = config('magiclink.disable_default_route', false);
+
+        if ($disableRegisterRoute) {
+            return;
+        }
+
+        $this->loadRoutesFrom(__DIR__.'/../routes/routes.php');
+    }
+
+    private function loadViewMagicLink() : void
+    {
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'magiclink');
+    }
+
+    private function offerPublishing(): void
+    {
+        if (! $this->app->runningInConsole()) {
+            return;
+        }
+
         $this->publishes([
             __DIR__.'/../config/magiclink.php' => config_path('magiclink.php'),
         ], 'config');
@@ -21,20 +50,8 @@ class MagicLinkServiceProvider extends ServiceProvider
             __DIR__.'/../databases/migrations' => database_path('migrations'),
         ], 'migrations');
 
-        if ($this->mustLoadRoute()) {
-            $this->loadRoutesFrom(__DIR__.'/../routes/routes.php');
-        }
-
-        // Views
-        $sourceViewsPath = __DIR__.'/../resources/views';
-        $this->loadViewsFrom($sourceViewsPath, 'magiclink');
         $this->publishes([
-            $sourceViewsPath => resource_path('views/vendor/magiclink'),
+            __DIR__.'/../resources/views' => resource_path('views/vendor/magiclink'),
         ], 'views');
-    }
-
-    protected function mustLoadRoute()
-    {
-        return ! config('magiclink.disable_default_route', false);
     }
 }
